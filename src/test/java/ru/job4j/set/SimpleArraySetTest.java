a6996ec922cad16b31c2e6223ae01e0ee087f1b9
aleksandrblagov <blagov13rus@gmail.com>
2023-12-14 21:03:58 +0300

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ src/test/java/ru/job4j/set/SimpleArraySetTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.set;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class SimpleArraySetTest {
+    @Test
+    void whenAddNonNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+    }
+
+    @Test
+    void whenAddSomeElementsNonNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.contains(1)).isFalse();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+        assertThat(set.contains(2)).isFalse();
+        assertThat(set.add(2)).isTrue();
+        assertThat(set.contains(2)).isTrue();
+        assertThat(set.add(2)).isFalse();
+        assertThat(set.contains(3)).isFalse();
+        assertThat(set.add(3)).isTrue();
+        assertThat(set.contains(3)).isTrue();
+        assertThat(set.add(3)).isFalse();
+    }
+
+    @Test
+    void whenAddNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.add(null)).isTrue();
+        assertThat(set.contains(null)).isTrue();
+        assertThat(set.add(null)).isFalse();
+    }
+
+    @Test
+    void whenCheckIteratorWithOneElement() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        set.add(1);
+        assertThat(set.iterator().hasNext()).isTrue();
+        assertThat(set.iterator().next()).isEqualTo(1);
+    }
+
+    @Test
+    void whenCheckIteratorWithoutElements() {
+        SimpleSet<String> set = new SimpleArraySet<>();
+        assertThat(set.iterator().hasNext()).isFalse();
+        assertThatThrownBy(() -> set.iterator().next())
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}
