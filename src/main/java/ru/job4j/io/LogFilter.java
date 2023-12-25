60642365fc5f18207dc98f1255b11f11f0979beb
aleksandrblagov <blagov13rus@gmail.com>
2023-12-25 09:55:23 +0300

    5.2. BufferedOutputStream [#252490 #468178]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -25,7 +25,7 @@ public class LogFilter {
         var data = filter();
         try (PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(out)))) {
             data.forEach(writer::println);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
