1a61a7c64e793328105c38dc91b9c7fbb55774a3
aleksandrblagov <blagov13rus@gmail.com>
2023-12-28 14:52:20 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,13 +7,33 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".properties")).forEach(System.out::println);
+        validation(args[0], args[1]);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validation(String initialFolder, String fileExtension) {
+        Path path = Paths.get(initialFolder);
+        if (!path.toFile().exists()) {
+            throw new IllegalArgumentException(
+                    String.format("Exist \"%s\"", path.toAbsolutePath())
+            );
+        }
+        if (!path.toFile().isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("Directory \"%s\"", path.toAbsolutePath())
+            );
+        }
+        if (!fileExtension.startsWith(".")) {
+            throw new IllegalArgumentException(
+                    String.format("Directory \"%s\"", fileExtension)
+            );
+        }
     }
 }
