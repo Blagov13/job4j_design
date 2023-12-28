14f7df5cecd817d83fa00158ea8d23e9f0816317
aleksandrblagov <blagov13rus@gmail.com>
2023-12-28 15:19:08 +0300

    11. Поиск дубликатов [#315066 #469195]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -2,12 +2,12 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
-        Files.walkFileTree(Paths.get("/Users/aleksandrblagov/job4j_design"), duplicatesVisitor);
+        Files.walkFileTree(Path.of("/Users/aleksandrblagov/job4j_design"), duplicatesVisitor);
         duplicatesVisitor.outDuplicates();
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,26 +5,26 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final Set<FileProperty> duplicates = new HashSet<>();
-    private final List<FileProperty> listDuplicates = new ArrayList<>();
+    private final Set<Path> duplicates = new HashSet<>();
+    private final Map<FileProperty, Path> fileDuplicate = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file,
                                      BasicFileAttributes attributes) throws IOException {
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
-        if (!duplicates.add(fileProperty)) {
-            listDuplicates.add(fileProperty);
+        if (fileDuplicate.containsKey(fileProperty)) {
+            duplicates.add(file);
+            duplicates.add(fileDuplicate.get(fileProperty));
+        } else {
+            fileDuplicate.put(fileProperty, file);
         }
         return super.visitFile(file, attributes);
     }
 
     public void outDuplicates() {
-        listDuplicates.forEach(duplicate -> System.out.printf(duplicate.getName()));
+        duplicates.forEach(System.out::println);
     }
 }
