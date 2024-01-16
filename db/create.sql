c0830ebbcc242f89c4444af814cdac11c3723e24
aleksandrblagov <blagov13rus@gmail.com>
2024-01-16 22:22:40 +0300

    2. Схема прав пользователи и роли. [#1093 #474088]

--- db/create.sql
+++ db/create.sql
@@ -1,31 +1,6 @@
-create table users (
-id serial primary key,
-name varchar(255),
-items_id int references items(id),
-);
-
-create table roles (
-id serial primary key,
-users_id int references users(id),
-name text
-);
-
 create table rules (
 id serial primary key,
 name text
-);
-
-create table rules_roles (
-id serial primary key,
-rules_id int references rules(id),
-roles_id int references roles(id)
-);
-
-create table items (
-id serial primary key,
-name text,
-attachs_id int references attachs(id),
-comment_id int references comment(id)
 );
 
 create table comment (
@@ -36,6 +11,31 @@ name text
 create table attachs (
 id serial primary key,
 name text
+);
+
+create table items (
+id serial primary key,
+name text,
+attachs_id int references attachs(id),
+comment_id int references comment(id)
+);
+
+create table users (
+id serial primary key,
+name varchar(255),
+items_id int references items(id)
+);
+
+create table roles (
+id serial primary key,
+users_id int references users(id),
+name text
+);
+
+create table rules_roles (
+id serial primary key,
+rules_id int references rules(id),
+roles_id int references roles(id)
 );
 
 create table states (
