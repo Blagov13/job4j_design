29b0d23f8fb974c6d0b61d06164ce3d53230fcca
aleksandrblagov <blagov13rus@gmail.com>
2024-01-18 09:19:28 +0300

    2. Схема прав пользователи и роли. [#1093 #474088]

--- db/insert.sql
+++ db/insert.sql
@@ -1,9 +1,10 @@
-insert into users(name, roles_id) values ('Aleksandr', 1);
 insert into roles(name) values ('admin');
 insert into rules(name) values ('full');
+insert into states(name) values ('done');
+insert into categories(name) values ('important');
+insert into users(name, roles_id) values ('Aleksandr', 1);
 insert into rules_roles (roles_id, rules_id) values (1, 1);
 insert into items (name, categories_id, states_id, users_id) values ('raport', 1, 1, 1);
 insert into comment(name, items_id) values ('fast', 1);
 insert into attachs(name, items_id) values ('file', 1);
-insert into states(name) values ('done');
-insert into categories(name) values ('important');
+
