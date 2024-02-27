df0f0ed2164bbbe0ba9ab1fc5c2e38a2b0457a79
aleksandrblagov <blagov13rus@gmail.com>
2024-02-27 15:38:34 +0300

    2. Парковка машин [#853 #483373] (Этап 2)

--- src/test/java/ru/job4j/ood/lsp/parking/VehicleTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/VehicleTest.java
@@ -8,16 +8,16 @@ import org.junit.jupiter.api.Test;
 @Disabled
 class VehicleTest {
     /*Тест для проверки возможности размещения легковой машины на месте для грузовых машин*/
-   /* @Test
+    /*@Test
     public void testParkVehicle_LightVehicleOnHeavySpot() {
         ParkingLot parkingLot = new ParkingLotImpl(5, 3);
         Vehicle lightVehicle = new Vehicle("Light", 1);
         assertFalse(parkingLot.parkVehicle(lightVehicle));
-    }
+    }*/
 
-    *//*Тест для проверки возможности размещения грузовой машины на
-    месте для грузовых машин и на смежных местах для легковых машин*//*
-    @Test
+    /*Тест для проверки возможности размещения грузовой машины на
+    месте для грузовых машин и на смежных местах для легковых машин*/
+    /*@Test
     public void testParkVehicle_HeavyVehicleOnHeavySpot() {
         ParkingLot parkingLot = new ParkingLotImpl(5, 3);
         Vehicle heavyVehicle = new Vehicle("Heavy", 2);
