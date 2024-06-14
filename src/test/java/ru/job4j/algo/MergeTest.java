0ce9ce02c26db7902ddb8d05d0a6f705d63f8535
aleksandrblagov <blagov13rus@gmail.com>
2024-06-14 15:08:32 +0300

    Сортировка слиянием [#504997 #500561]

--- src/test/java/ru/job4j/algo/MergeTest.java
+++ src/test/java/ru/job4j/algo/MergeTest.java
@@ -10,4 +10,10 @@ class MergeTest {
         int[] array = {10, 4, 6, 4, 8, -13, 2, 3};
         assertThat(Merge.mergesort(array)).containsExactly(-13, 2, 3, 4, 4, 6, 8, 10);
     }
+
+    @Test
+    void whenSortedThenNotEven() {
+        int[] array = {2, 5, 7, -8, 6, 11, 17};
+        assertThat(Merge.mergesort(array)).containsExactly(-8, 2, 5, 6, 7, 11, 17);
+    }
 }
