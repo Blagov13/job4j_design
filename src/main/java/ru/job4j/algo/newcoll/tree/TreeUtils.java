22b00fc43b732bc2917a39edcfd3e51fd9dd24c7
aleksandrblagov <blagov13rus@gmail.com>
2024-07-06 19:23:52 +0300

    Обход дерева в ширину [#504972]

---
+++ src/main/java/ru/job4j/algo/newcoll/tree/TreeUtils.java
@@ -0,0 +1,53 @@
+package ru.job4j.algo.newcoll.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class TreeUtils<T> {
+
+    /**
+     * Метод выполняет обход дерева и считает количество узлов
+     *
+     * @param root корневой узел дерева
+     * @return количество узлов
+     * @throws IllegalArgumentException если root является null
+     */
+    public int countNode(Node<T> root) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        Queue<Node<T>> queue = new LinkedList<>();
+        queue.add(root);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            Node<T> current = queue.poll();
+            count++;
+            queue.addAll(current.getChildren());
+        }
+        return count;
+    }
+
+    /**
+     * Метод выполняет обход дерева и возвращает коллекцию ключей узлов дерева
+     *
+     * @param root корневой узел
+     * @return коллекция с ключами, реализующая интерфейс Iterable<E>
+     * @throws IllegalArgumentException если root является null
+     */
+    public Iterable<T> findAll(Node<T> root) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        List<T> result = new ArrayList<>();
+        Queue<Node<T>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node<T> current = queue.poll();
+            result.add(current.getValue());
+            queue.addAll(current.getChildren());
+        }
+        return result;
+    }
+}
